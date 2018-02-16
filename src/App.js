c95226a9cef3cf16fc1eddaa7a8f02821cf8c707
Gaya Kessler <gaya.kessler@gmail.com>
2018-02-16 08:06:47 +0100

    basic form looks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import { Container, Menu, Header, Input } from 'semantic-ui-react';
 
 import calc from './calc';
 
 import './App.css';
 
-function Field({ name, label, value, onChange }) {
+function Field({ name, label, value, rightLabel, onChange }) {
+  const inputLabel = rightLabel ? {
+    label: { basic: true, content: rightLabel },
+    labelPosition: 'right',
+  } : {};
+
   return (
     <div className="App__field">
-      <label htmlFor={name}>{label}</label>
-      <input
+      <Header as="h4" style={{ margin: 0 }}>{label}</Header>
+      <Input
           name={name}
           id={name}
           value={value}
           onChange={onChange}
+          {...inputLabel}
           type="number"
+          style={{ width: '100%' }}
       />
     </div>
   );
@@ -55,32 +63,42 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Field
-            name="max"
-            label="Max"
-            value={max}
-            onChange={(e) => this.update('max', e.target.value)}
-        />
-        <Field
-            name="ratio"
-            label="Start ratio"
-            value={ratio}
-            onChange={(e) => this.update('ratio', e.target.value)}
-        />
-        <Field
-            name="sets"
-            label="# of sets"
-            value={sets}
-            onChange={(e) => this.update('sets', e.target.value)}
-        />
-        <Field
-            name="smallestIncrement"
-            label="Smallest increment"
-            value={smallestIncrement}
-            onChange={(e) => this.update('smallestIncrement', e.target.value)}
-        />
+        <Menu inverted className="App__top">
+          <Container text style={{ justifyContent: 'center' }}>
+            <Header inverted>Test your might</Header>
+          </Container>
+        </Menu>
+        <Container text>
+          <Field
+              name="max"
+              label="Max"
+              value={max}
+              onChange={(e) => this.update('max', e.target.value)}
+              rightLabel="kg"
+          />
+          <Field
+              name="ratio"
+              label="Start ratio"
+              value={ratio}
+              onChange={(e) => this.update('ratio', e.target.value)}
+              rightLabel="%"
+          />
+          <Field
+              name="sets"
+              label="# of sets"
+              value={sets}
+              onChange={(e) => this.update('sets', e.target.value)}
+          />
+          <Field
+              name="smallestIncrement"
+              label="Smallest increment"
+              value={smallestIncrement}
+              onChange={(e) => this.update('smallestIncrement', e.target.value)}
+              rightLabel="kg"
+          />
 
-        <div>{this.setsWeight(max, sets, weightPerSet).join(', ')}</div>
+          <div>{this.setsWeight(max, sets, weightPerSet).join(', ')}</div>
+        </Container>
       </div>
     );
   }
